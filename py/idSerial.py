c725f9fb886bd59755de46cec99493d027de16a5
KiyomiHan <1033138571@qq.com>
2018-04-03 11:35:08 +0800

    id

--- py/idSerial.py
+++ py/idSerial.py
@@ -3,7 +3,10 @@ import serial.tools.list_ports
 import re
 
 def getPort():
-    port_list = serial.tools.list_ports.comports()
+    try:
+        port_list = serial.tools.list_ports.comports()
+    except:
+        print("IDNONE")
     port = 0
     for ports in port_list:
         if (re.match('/dev/ttyUSB0', ports.device)):
@@ -13,12 +16,17 @@ def getPort():
 def readID():
     port  = getPort()
     print("port----",port)
-    ser = serial.Serial(port, 9600, timeout=1)
+    try:
+        ser = serial.Serial(port, 9600, timeout=1)
+    except:
+        print("IDNONE")
     idNum = ""
     while (len(idNum) == 0):
-        idNum = (ser.readline()).decode('utf-8')  # 原始串口数据为bytes，需解码成str(utf-8)
-
-    print("idnum--",idNum)
+        try:
+            idNum = (ser.readline()).decode('utf-8')  # 原始串口数据为bytes，需解码成str(utf-8)
+        except:
+            print("IDNONE")
+    # print("idnum--",idNum)
     return idNum
 
 def main():
